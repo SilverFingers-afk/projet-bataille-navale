4cac85d8eab372e6cf078b49cad1f36caab39a30
SilverFingers-afk <ferdinand.coatmellec@gmail.com>
2025-10-27 18:41:32 +0100

    User story  "Plouf dans l'eau"

--- story_grille.py
+++ story_grille.py
@@ -2,14 +2,22 @@
 Utilisateur : un joueur
 Story : On veut pouvoir gérer les tirs de l'adversaire
 Actions :
-créer une grille à 5 lignes et 8 colonnes
+
 afficher la grille à l'écran
 demande à l'utilisateur de rentrer deux coordonnées x et y
 tier à l'endroit indiqué sur la grille
 retour en 2"""
 
-joeur = 3
-for i in range(3,4):
-    joeur += 3
+"""créer une grille à 5 lignes et 8 colonnes"""
+nb_coups = 0
+nb_max = 30
 
-print(joeur)
+while nb_coups != nb_max:
+    print(grille)
+
+    coordonnees = input("Choisez une coordonnée pour le tir")
+
+    if True:
+        """Tire à l'endroit et change d'icône selon raté ou touché"""
+    else:
+        """message d'erreur: coordonnées incorrectes, on reboucle sans compte le coup"""
