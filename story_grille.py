a1ca526a070368e617c3f65ccfaaeaaec8604574
SilverFingers-afk <ferdinand.coatmellec@gmail.com>
2025-11-09 01:28:32 +0100

    completing user story

--- story_grille.py
+++ story_grille.py
@@ -3,17 +3,18 @@ Utilisateur : un joueur
 Story : On veut pouvoir gérer les tirs de l'adversaire
 Actions :
 """
-
-"""créer une grille à 5 lignes et 8 colonnes"""
+from grille import Grille
+grille = Grille(5,8)
 nb_coups = 0
 nb_max = 30
 
 while nb_coups != nb_max:
-    print(grille(5))
+    print(grille)
 
     coordonnees = input("Choisez une coordonnée pour le tir (chiffres séparés d'un espace)")
-
-    if True:
-        """Tire à l'endroit et change d'icône selon raté ou touché"""
+    
+    if 0 < ord(coordonnees[0])-64 < 9 and  0 < (int(coordonnees[2])) < 6 :
+        grille.tirer(int(coordonnees[2]), ord(coordonnees[0])-64 )
+        nb_coups+=1
     else:
-        """message d'erreur: coordonnées incorrectes, on reboucle sans compte le coup"""
+        print("Erreur : coordonnées non valides")
